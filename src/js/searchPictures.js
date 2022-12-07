093e514f578dfc428edc9747d554171789b704a4
kszczerbowski <kszczerbowski223@gmail.com>
2022-12-07 21:09:10 +0100

    css and gallery update
    create markup from database on input, pictures positioning

--- src/js/searchPictures.js
+++ src/js/searchPictures.js
@@ -2,18 +2,45 @@ import Notiflix from 'notiflix';
 const axios = require('axios').default;
 import { pictureInput } from '../index';
 
+const gallery = document.querySelector('div.gallery');
+let pageNumber = 1;
+const markupArr = [];
+
 export async function searchPictures() {
   try {
-    const response = await axios.get('https://pixabay.com/api/', {
-      params: {
-        key: '31853975-bc1b1ba443a9213885c0622f6',
-        q: pictureInput.value,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      }
+    const searchParams = new URLSearchParams({
+      key: '31853975-bc1b1ba443a9213885c0622f6',
+      q: pictureInput.value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: pageNumber,
+      per_page: 40,
     });
-    console.log(response.data.hits); // zwraca 3 keys'y, interesuje Cię 3ci, czyli hits
+    const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
+    console.log(response.data); // zwraca 3 keys'y, interesuje Cię 3ci, czyli hits
+    response.data.hits.forEach(image => {
+      markupArr.push(`
+      <div class="photo-card">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${image.likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${image.views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${image.comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${image.downloads}
+          </p>
+        </div>
+      </div>
+      `)
+      gallery.insertAdjacentHTML('beforeend', markupArr.join(''));
+    })
   } catch (error) {
     console.error(error);
   }
