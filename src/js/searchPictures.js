788b99d175eb1c47d28a11571eb09aa237f778dc
kszczerbowski <kszczerbowski223@gmail.com>
2022-12-13 00:10:20 +0100

    infinite scrolls, arrows and dragons

--- src/js/searchPictures.js
+++ src/js/searchPictures.js
@@ -9,9 +9,12 @@ const axios = require('axios').default;
 const pictureInput = document.querySelector('input[name=searchQuery]');
 const gallery = document.querySelector('div.gallery');
 const loadMoreBtn = document.querySelector('button.load-more');
+const scrollUpArrow = document.querySelector('img.scroll-up-arrow');
+const dragon = document.querySelector('img.dragon');
 
 let pageNumber = 1;
 let lightbox;
+let infiniteScrollMode = false;
 
 pictureInput.addEventListener('input', debounce(() => {
     cleanGallery();
@@ -84,11 +87,12 @@ async function searchPictures() {
           behavior: "smooth",
         });
       }
-      loadMoreBtn.classList.remove('hidden');
+      scrollUpArrow.classList.remove('hidden');
+      if (!infiniteScrollMode) loadMoreBtn.classList.remove('hidden');
     } else {
       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     }
-    if (document.querySelectorAll('div.photo-card').length === response.data.totalHits && response.data.totalHits !== 0) {
+    if (document.querySelectorAll('a.photo-card').length === response.data.totalHits && response.data.totalHits !== 0) {
       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
       loadMoreBtn.classList.add('hidden');
     }
@@ -98,11 +102,52 @@ async function searchPictures() {
 }
 
 loadMoreBtn.addEventListener('click', () => {
+  if (pageNumber === 1) Notiflix.Notify.info('No love for the load more button? Press ctrl+I to enter the Infinity Scroll Mode!')
   pageNumber++;
   lightbox.refresh();
   searchPictures();
 });
 
+window.addEventListener('scroll',()=>{
+  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight && infiniteScrollMode){
+    pageNumber++;
+    lightbox.refresh();
+    searchPictures();
+  }
+})
+
+document.addEventListener("keydown", event => {
+  if ((event.ctrlKey || event.metaKey) && event.code === "KeyI") {
+    loadMoreBtn.classList.add('hidden');
+    infiniteScrollMode = true;
+  }
+});
+
+document.addEventListener("keydown", event => {
+  if ((event.ctrlKey || event.metaKey) && event.code === "KeyD") {
+    event.preventDefault();
+    dragon.classList.toggle('go-left');
+    dragon.classList.toggle('go-right');
+    setTimeout(() => {
+      dragon.classList.toggle('rotated')
+    }, 2000)
+  }
+});
+
 document.querySelector('form.search-form').addEventListener('submit', (event) => {
   event.preventDefault();
 })
+
+scrollUpArrow.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+  scrollUpArrow.classList.add('hidden');
+})
+
+document.addEventListener("mousewheel", (event) => {
+  if(event.wheelDelta < 0) scrollUpArrow.classList.remove('hidden');
+})
+
